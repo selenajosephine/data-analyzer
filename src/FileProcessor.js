3ffe1bbd9ba2962ceb2ffcca1301ccdec328f79b
selenajosephine <43022347+selenajosephine@users.noreply.github.com>
2019-05-22 22:56:14 +0530

    heading processing changes

--- src/FileProcessor.js
+++ src/FileProcessor.js
@@ -1,8 +1,9 @@
 const readline = require('readline');
 const fs = require('fs')
 
+let title = []
 
-
+// method to read input from user
 const Ask = (questions) => {
     return new Promise(async resolve => {
         let rl = readline.createInterface(process.stdin, process.stdout);
@@ -11,16 +12,32 @@ const Ask = (questions) => {
     })
 }
 
-
-const readFile = (path) =>{
-    return new Promise((resolve,reject) =>{
-        fs.readFile(path,{encoding:'utf8'}, (err, data)=>{
-            err?reject(err):resolve(data);
+// method to read the file
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+            err ? reject(err) : resolve(data);
         });
     });
 }
 
+// method to split the file into title and 
+const processFile = (file) => {
+    try {
+        titleTemp = file.split("\n")
+        title = titleTemp[0].split("\t")
+        var data = titleTemp;
+        data.shift()
+        
+        console.log("Do you have "+title.length+ " columns?  (y/n)")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
 module.exports = {
     askQuestions: Ask,
-    readFile: readFile
+    readFile: readFile,
+    processFile: processFile
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const { askQuestions, readFile } = require('./FileProcessor');
+const { askQuestions, readFile,processFile } = require('./FileProcessor');
 const filelocationQuestion = 'Enter your file location  '
 
 askQuestions(filelocationQuestion)
     .then(filename => {
+
         if (filename.includes(".txt") || filename.includes(".csv")) {
-            readFile(filename).then(file =>{
-               console.log(file)
-            }).catch(err =>{
-                console.log(`Check the file location`);
-                process.exit(1)
-            })
+            readFile(filename)
+                .then(file =>processFile(file))
+                .catch(err =>{console.log(`Check the file location`);process.exit(1);})
+                
         }
         else{
             console.log(`Only .txt and .csv files are accepted`)
+            filename="";
+            //askQuestions(filelocationQuestion)
             process.exit(1)
         }
 
