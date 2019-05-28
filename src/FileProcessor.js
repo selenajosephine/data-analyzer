1580739236e4fef6cc1de98ebdced38467703342
selenajosephine <43022347+selenajosephine@users.noreply.github.com>
2019-05-28 13:05:59 +0530

    read group by values changes

--- src/FileProcessor.js
+++ src/FileProcessor.js
@@ -1,15 +1,14 @@
 const readline = require('readline');
 const fs = require('fs')
 
-let title = []
+let title = [];
+let groupby = [];
 
 // method to read input from user
 const Ask = (questions) => {
     return new Promise(async resolve => {
         let rl = readline.createInterface({ input: process.stdin, output: process.stdout, terminal: false });
-        let results = await new Promise(resolve => {
-            rl.question(questions, answer => resolve(answer))
-        })
+        let results = await new Promise(resolve => { rl.question(questions, answer => resolve(answer)) })
         resolve(results)
     })
 }
@@ -17,9 +16,7 @@ const Ask = (questions) => {
 // method to read the file
 const readFile = (path) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
-            err ? reject(err) : resolve(data);
-        });
+        fs.readFile(path, { encoding: 'utf8' }, (err, data) => { err ? reject(err) : resolve(data); });
     });
 }
 
@@ -27,18 +24,20 @@ const readFile = (path) => {
 const processFile = (file) => {
     try {
         titleTemp = file.split("\n")
-        title = titleTemp[0].split("\t")
+        title = titleTemp[0].trim().split("\t")
         var data = titleTemp;
         data.shift()
 
-        Ask("Do you have " + title.length + " columns?  (y/n)").then(response => {
-            console.log(`inside response chunk`)
+        Ask("Do you have " + title.length + " columns?  (y/n)  ").then(response => {
             if (response == "Y" || response == "y") {
                 var jsonData = convertToJson(data, title)
                 return jsonData
             }
+            // TO-DO
             else Ask("Then how many columns are there?  ").then(columncount => { console.log(columncount) })
-        }).then(jsonData => { console.log(jsonData) })
+        }).then(jsonData => {
+            sortData(jsonData)
+        })
     }
     catch (err) {
         console.log(err)
@@ -58,6 +57,26 @@ var convertToJson = (data, title) => {
     return json
 }
 
+
+var sortData = jsonData => {
+    var groupBy = [];
+    groupBy = groupByColumns()
+}
+
+var groupByColumns = () => {
+    console.log("Your columns are")
+    title.forEach((value, index) => {
+        console.log(`${index + 1}. ${value}`)
+    })
+    Ask("Which columns do you want to group by? Enter the column number separated by commas (,)  ").then(response =>{
+        var temp = response.split(",")
+        for(var i = 0; i<temp.length;i++)
+        {
+            groupby.push(title[temp[i]-1])
+        }
+        
+    })
+}
 
 module.exports = {
     askQuestions: Ask,
